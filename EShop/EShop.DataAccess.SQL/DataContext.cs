01a29ef22d17a0b814f7a4968f24598bad045d0b
kyleHaldane <kyle.haldane@gmail.com>
2020-07-19 16:34:10 -0400

    Added Baket Model

--- EShop/EShop.DataAccess.SQL/DataContext.cs
+++ EShop/EShop.DataAccess.SQL/DataContext.cs
@@ -18,5 +18,7 @@ namespace EShop.DataAccess.SQL
 
         public DbSet<Product> Products { get; set; }   
         public DbSet<ProductCategory> ProductCtegories { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
     }
 }
